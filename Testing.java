47688ddf94959629fedb55364bc90838fdf5c2f8
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-01-29 14:56:36 -0500

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,4 +1,4 @@
-public class testing {
+public class Testing {
     public void methodWithLongLine() {
         String aVeryLongString = "This should be ok!";
         System.out.println(aVeryLongString);
