0573c9e3ff9fd818c6378cdfb9b479f6983d54d5
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-01-29 14:49:13 -0500

    Update testing.java

--- testing.java
+++ testing.java
@@ -1,6 +1,6 @@
 public class ExampleClassWithLongLine {
     public void methodWithLongLine() {
-        String aVeryLongString = "This is a very long string that exceeds the maximum line length specified by Checkstyle. This line should trigger a violation.";
+        String aVeryLongString = "This should be ok!";
         System.out.println(aVeryLongString);
     }
 }
