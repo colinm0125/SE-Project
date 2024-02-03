d87e6c0fcb414bb9fd96c0af6ece915050e85645
colinm0125 <colinm0125@gmail.com>
2024-02-03 15:45:58 -0500

    Update DataToFromCompute.java

--- DataToFromCompute.java
+++ DataToFromCompute.java
@@ -1,6 +1,6 @@
 public interface DataToFromCompute {
     //A method to read the data from the source
-    SourceType ReadData();    
+    SourceType readData();    
     //A method to write to the output source
-    DataKey WriteData(SourceType Source);
+    DataKey writeData(SourceType Source);
 }
