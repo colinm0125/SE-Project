d30d077fa43718ebc29e7e5dd2d78eb8b3c5f74b
colinm0125 <colinm0125@gmail.com>
2024-02-08 11:03:27 -0500

    Create DataToFromComputeImpl.java

---
+++ DataToFromComputeImpl.java
@@ -0,0 +1,13 @@
+public class DataToFromComputeImpl implements DataToFromCompute{
+    
+    @Override
+    public RawData readData(SourceType sourceType){
+        return null;
+        //placeholder implementation
+    }
+    @Override
+    public DataKey writeData(ComputeResults compute, SourceType sourceType){
+        return null;
+        //placeholder implementation
+    }
+}
