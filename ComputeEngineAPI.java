7b9d1d7bdc6704df30ea33b31bf43f8bd37414dd
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-08 10:30:21 -0500

    Updated ComputeEngineAPI.java (#13)

--- ComputeEngineAPI.java
+++ ComputeEngineAPI.java
@@ -1,6 +1,4 @@
 public interface ComputeEngineAPI {
     //inputs raw data and fetches results/error codes
     ComputeResults compute(RawData data);
-    //requests for results after computation
-    ProcessedData result();
 }
