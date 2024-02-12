abe980bc0541a5e46784abb4b619fd08e38fc3b5
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-12 14:35:38 -0500

    Uploaded API implementations and object classes (#20)
    * Add files via upload
    
    * Deleted extr file
    
    * Deleted extra file
    
    * Added contructors
    
    * Deleted dupe

--- src/ComputeEngineAPI.java
+++
@@ -1,4 +0,0 @@
-public interface ComputeEngineAPI {
-    //inputs raw data and fetches results/error codes
-    ComputeResults compute(RawData data);
-}
---
+++ src/UserToComputeAPI.java
@@ -0,0 +1,55 @@
+public class UserToComputeAPI implements UserToCompute{
+    //Object instances
+    SourceType source;
+    OutputDest dest;
+    UserDelimeter inputDelim, outputDelim;
+    RawData input;
+
+    //Constructors
+    public UserToComputeAPI(SourceType source, OutputDest dest, UserDelimeter inputDelim, UserDelimeter outputDelim, RawData input) {
+        this.source = source;
+        this.dest = dest;
+        this.inputDelim = inputDelim;
+        this.outputDelim = outputDelim;
+        this.input = input;
+    }
+    public UserToComputeAPI() {
+
+    }
+
+    //Method to set input source
+    public SourceType setInputSource(SourceType source) {
+
+        return source;
+    }
+
+    //Method to set output destination for prime summation
+    public OutputDest setOutputDest(OutputDest dest){
+
+        return dest;
+    }
+
+    //Method to set custom delimeters
+    public UserDelimeter setDelimeter(UserDelimeter inputDelim, UserDelimeter outputDelim) {
+
+        return null;
+    }
+
+    //Method overload
+    public UserDelimeter setDelimeter(UserDelimeter delim) {
+        setDelimeter(delim, delim);
+        return delim;
+    }
+
+    //Method to call the computation prime summations
+    public SourceType getOutput() {
+
+        return null;
+    }
+
+    //Computes and returns the data based off of compute engine
+    public ComputeResults computeRequest(RawData input) {
+
+        return null;
+    }
+}
