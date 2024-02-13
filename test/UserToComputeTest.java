2182b569f44f0c1836bc8c91c8ee0ecf56bb145e
Colin <134108049+colinm0125@users.noreply.github.com>
2024-02-13 16:27:08 -0500

    Update UserToComputeTest.java

--- test/UserToComputeTest.java
+++ test/UserToComputeTest.java
@@ -2,29 +2,38 @@ import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 
+//Mock objects
+private SourceType sourceMock=mock(SourceType.class);
+private OutputDest outputMock=mock(OutputDest.class);
+private UserDelimeter inputDelimMock=mock(UserDelimeter.class);
+private UserDelimeter outputDelimMock=mock(UserDelimeter.class);
+private UserToComputeAPI testAPI;
+
 
 public class UserToComputeTest implements UserToCompute {
-    public void testUserToCompute(){
-        //Creating mock objects
-        SourceType sourceMock=mock(SourceType.class);
-        OutputDest outputMock=mock(OutputDest.class);
-        UserDelimeter inputDelimMock=mock(UserDelimeter.class);
-        UserDelimeter outputDelimMock=mock(UserDelimeter.class);
-        
-
-        UserToCompute userToCompute= new UserToComputeImpl();
-        //Create instance of the component
-
-        //Set behavior of mock objects
-        when(sourceMock.getData()).thenReturn("Mocked Source Data");
-        when(outputMock.getData()).thenReturn("Mocked output data");
-
-        userToCompute.setInputSource(sourceMock);
-        userToCompute.setOutputDest(outputMock);
-        userToCompute.setDelimeter(inputDelim, outputDelimMock);
-
-        verify(sourceMock).getData();
-        verify(outputMock).getData();
-    
+     public void testSetInputSource(){
+        when(testAPI.setInputSource(any(SourceType.class))).thenReturn ("Setting Input Success");
+        testAPI.testSetInputSource(sourceMock);
     }
+
+    public void testSetOutputDest(){
+        when(testAPI.setOutputDest(any(OutputDest.class))).thenReturn("Setting Output Dest Success");
+        testAPI.testSetOutputDest(outputMock);
+
+    }
+
+    public void testSetDelimeter(){
+        when(testAPI.setDelimeter(any(UserDelimeter.class))).thenReturn("Setting delimeter success");
+        testAPI.testSetDelimeter(inputDelimMock,outputDelimMock);
+    }
+
+    public void testGetOutput(){
+        when(testAPI.getOutput()).thenReturn("Output :)");
+        testAPI.testGetOutput;
+    }
+
+    public void testComputeRequest(){
+        when(testAPI.computeRequest(any(RawData.class))).thenReturn("Compute Request success");
+        testAPI.testComputeRequest;        
 }
+
