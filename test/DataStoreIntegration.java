80f91e83378cc19976841b39f52361968c4405f4
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-13 17:33:52 -0500

    Added file (#34)
    * Add files via upload
    
    * Update DataStoreIntegration.java
    
    Fixed the misspelling of "main"
    
    * Update DataStoreIntegration.java
    
    Forgot a ; on line 8
    
    ---------
    
    Co-authored-by: Colin <134108049+colinm0125@users.noreply.github.com>

---
+++ test/DataStoreIntegration.java
@@ -0,0 +1,10 @@
+class DataStoreIntegration {
+    public static void main(String args[]) throws Exception {
+        DataToFromComputeAPI api;
+        OutputConfig out;
+        InputConfig in;
+
+        api.readData(null);
+        api.writeData(in, null);
+    }
+}
