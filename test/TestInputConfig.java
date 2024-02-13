956d26b88bf068486b9b4f87120b0fa6c687489f
Colin <134108049+colinm0125@users.noreply.github.com>
2024-02-13 12:50:52 -0500

    Update TestInputConfig.java
    Base For the InMemoryConfig tests

--- test/TestInputConfig.java
+++ test/TestInputConfig.java
@@ -1,1 +1,20 @@
+import java.util.List;
+
+public class InMemoryConfig {
+    private List<Integer> inputConfig;
+    private List<String> outputConfig;
+
+    public InMemoryConfig() {
+        this.inputConfig = new List<Integer>();
+        this.outputConfig = new List<String>();
+    }
+
+    public List<Integer> getInputConfig() {
+        return inputConfig;
+    }
+
+    public List<String> getOutputConfig() {
+        return outputConfig;
+    }
+}
 
