df26fc1facd17ab821b39f93c2a9e2d15bc94b4b
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-13 17:15:32 -0500

    Add files via upload

---
+++ test/DataStoreIntegration.java
@@ -0,0 +1,10 @@
+class DataStoreIntegration {
+    public static void mian(String args[]) throws Exception {
+        DataToFromComputeAPI api;
+        OutputConfig out;
+        InputConfig in;
+
+        api.readData(null);
+        api.writeData(in, null)
+    }
+}
