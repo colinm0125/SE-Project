4f6d834a9f102d0e1f800283a272b4cbadc88635
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-13 16:54:05 -0500

    Update UserToComputeTest.java

--- test/UserToComputeTest.java
+++ test/UserToComputeTest.java
@@ -3,41 +3,36 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 
 public class UserToComputeTest {
-     //Mock objects
-     private SourceType sourceMock;
-     private OutputDest outputMock;
-     private UserDelimeter inputDelimMock; 
-     private UserDelimeter outputDelimMock;
      private UserToComputeAPI testAPI;
      
      public void testSetInputSource() throws Exception {
-        sourceMock=Mockito.mock(SourceType.class);
+        SourceType sourceMock=Mockito.mock(SourceType.class);
         when(testAPI.setInputSource(any(SourceType.class))).thenReturn("Setting Input Success");
         testAPI.setInputSource(sourceMock);
     }
 
     public void testSetOutputDest() throws Exception {
-        outputMock=Mockito.mock(OutputDest.class);
+        OutputDest outputMock=Mockito.mock(OutputDest.class);
         when(testAPI.setOutputDest(any(OutputDest.class))).thenReturn("Setting Output Dest Success");
         testAPI.setOutputDest(outputMock);
 
     }
 
     public void testSetDelimeter() throws Exception {
-        inputDelimMock=Mockito.mock(UserDelimeter.class); 
-        outputDelimMock=Mockito.mock(UserDelimeter.class); 
+        UserDelimeter inputDelimMock=Mockito.mock(UserDelimeter.class); 
+        UserDelimeter outputDelimMock=Mockito.mock(UserDelimeter.class); 
         when(testAPI.setDelimeter(any(UserDelimeter.class))).thenReturn("Setting delimeter success");
         testAPI.setDelimeter(inputDelimMock,outputDelimMock);
     }
 
     public void testGetOutput() throws Exception {
-           
         when(testAPI.getOutput()).thenReturn("Output :)");
-        testAPI.getOutput;
+        testAPI.getOutput();
     }
 
     public void testComputeRequest() throws Exception {
         when(testAPI.computeRequest(any(RawData.class))).thenReturn("Compute Request success");
-        testAPI.computeRequest;        
+        testAPI.computeRequest();
+    }
 }
 
