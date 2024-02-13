abd309a8834706d6f66f5870661d768040daa24c
Colin <134108049+colinm0125@users.noreply.github.com>
2024-02-13 16:49:42 -0500

    Update UserToComputeTest.java

--- test/UserToComputeTest.java
+++ test/UserToComputeTest.java
@@ -10,33 +10,33 @@ public class UserToComputeTest {
      private UserDelimeter outputDelimMock;
      private UserToComputeAPI testAPI;
      
-     public void testSetInputSource(){
+     public void testSetInputSource() throws Exception {
         sourceMock=Mockito.mock(SourceType.class);
-        when(testAPI.setInputSource(any(SourceType.class))).thenReturn ("Setting Input Success");
+        when(testAPI.setInputSource(any(SourceType.class))).thenReturn("Setting Input Success");
         testAPI.setInputSource(sourceMock);
     }
 
-    public void testSetOutputDest() {
+    public void testSetOutputDest() throws Exception {
         outputMock=Mockito.mock(OutputDest.class);
         when(testAPI.setOutputDest(any(OutputDest.class))).thenReturn("Setting Output Dest Success");
         testAPI.setOutputDest(outputMock);
 
     }
 
-    public void testSetDelimeter() {
+    public void testSetDelimeter() throws Exception {
         inputDelimMock=Mockito.mock(UserDelimeter.class); 
         outputDelimMock=Mockito.mock(UserDelimeter.class); 
         when(testAPI.setDelimeter(any(UserDelimeter.class))).thenReturn("Setting delimeter success");
         testAPI.setDelimeter(inputDelimMock,outputDelimMock);
     }
 
-    public void testGetOutput() {
+    public void testGetOutput() throws Exception {
            
         when(testAPI.getOutput()).thenReturn("Output :)");
         testAPI.getOutput;
     }
 
-    public void testComputeRequest() {
+    public void testComputeRequest() throws Exception {
         when(testAPI.computeRequest(any(RawData.class))).thenReturn("Compute Request success");
         testAPI.computeRequest;        
 }
