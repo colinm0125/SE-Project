46e8a2dcb25f2a333c0d920f3905b6df7c74d6d3
colinm0125 <colinm0125@gmail.com>
2024-02-13 14:31:44 -0500

    Completion of TestInputConfig file.

--- test/TestInputConfig.java
+++ test/TestInputConfig.java
@@ -1,1 +1,14 @@
+import java.util.List;
+import java.util.LinkedList;
 
+public class TestInputConfig{
+    static List<Integer> inputData= new LinkedList<>();
+    
+    public static void setInputData(List<Integer> data) throws Exception{
+        inputData=data;
+    }
+
+    public static List<Integer> getInputData(){
+        return inputData;
+    }
+}
