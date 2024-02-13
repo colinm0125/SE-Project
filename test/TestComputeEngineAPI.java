5e244d54d74f814b87fb3b8d00e4fa6157105717
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-13 11:47:41 -0500

    Update TestComputeEngineAPI.java

--- test/TestComputeEngineAPI.java
+++ test/TestComputeEngineAPI.java
@@ -5,13 +5,15 @@ import static org.mockito.Mockito.any;
 
 public class TestComputeEngineAPI {
 
+    ComputeEngineAPI testAPI;
+    
     @Test
     public void testComputeEngineAPI() throws Exception {
         RawData data = Mockito.mock(RawData.class);
-
-        when(data).thenReturn("Data computed");
-
         ComputeEngineAPI testAPI = new ComputeEngineAPI();
+
+        when(testAPI.compute(any(data))).thenReturn("Data computed");
+ 
         testAPI.compute(data);
     }
 }
