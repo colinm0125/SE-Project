932c0fe38d8fb9169b697007de23181013b02dce
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-13 12:33:15 -0500

    Add files via upload

---
+++ test/TestDataToFromComputeAPI.java
@@ -0,0 +1,27 @@
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+
+public class TestDataToFromComputeAPI {
+    private DataToFromComputeAPI testAPI;
+
+    @Test
+    public void testWrite() throws Exception {
+        SourceType mockSource = Mockito.mock(SourceType.class);
+        ComputeResults mockResult = Mockito.mock(ComputeResults.class);
+
+        when(testAPI.writeData(any(SourceType.class), any(ComputeResults.clas))).thenReturn("Key made");
+
+        testAPI.writeData(mockSource, mockResult);
+    }
+
+    @Test
+    public void testRead() throws Exception {
+        SourceType mockSource = Mocktio.mock(SourceType.class);
+
+        when(testAPI.readData(any(SourceType.class))).thenReturn("Data read");
+
+        testAPI.readData(mockSource);
+    }
+}
