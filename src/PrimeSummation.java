536e8ec528fb119063c68e5c6bb70c4ebc9debd2
colinm0125 <colinm0125@gmail.com>
2024-02-21 15:41:07 -0500

    MORE CHECKSTYLE
    RAHHHHH

--- src/PrimeSummation.java
+++ src/PrimeSummation.java
@@ -30,8 +30,7 @@ public class PrimeSummation {
                     }
                 }  
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
