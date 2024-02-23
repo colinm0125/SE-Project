0e297f5afc9b4091e35addc69d1c6774a14d8ed2
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-02-22 19:55:42 -0500

    Add files via upload

---
+++ src/DataToFromComputeAPI.java
@@ -0,0 +1,25 @@
+public class DataToFromComputeAPI implements DataToFromCompute {
+    //Object Instances
+    SourceType sourceType;
+    ComputeResults compute;
+
+    //Constructor
+    public DataToFromComputeAPI(SourceType sourceType, ComputeResults compute) {
+        this.sourceType = sourceType;
+        this.compute = compute;
+    }
+    public DataToFromComputeAPI() {
+    }
+
+    //A method to read the data from the source
+    public RawData readData(SourceType sourceType) {
+
+        return null;
+    }
+
+    //A method to write to the output source
+    public DataKey writeData(ComputeResults compute, SourceType sourceType) {
+
+        return null;
+    }
+}
