ce020126f0009fdf484a54966e24b2d32e14825f
Colin <134108049+colinm0125@users.noreply.github.com>
2024-02-22 19:11:52 -0500

    Create ComputationTest.java
    Quick test for the performance of the computation. Definitely might need some work, if you are able to. I just want to commit what i have here before going to work

---
+++ test/ComputationTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PrimeSummationTest {
+
+    @Test
+    public void testPrimeSumPerformance() {
+        // Define input parameters for the test
+        int num = 100; // Adjust the input number as needed
+        List<Integer> sums = new ArrayList<>();
+        
+        // Measure the execution time of the primeSum method
+        long startTime = System.currentTimeMillis();
+        PrimeSummation.primeSum(num, 1, sums);
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        
+        // Print out the execution time
+        System.out.println("Execution time for primeSum method with input " + num + ": " + executionTime + " milliseconds");
+        
+        // Assert that the execution time is within an acceptable range (optional)
+        assertTrue("Execution time should be less than 100 milliseconds", executionTime < 100);
+    }
+}
