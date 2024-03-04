9e7421418f46a8c318f94b459eb14c4c2da7c5a5
colinm0125 <colinm0125@gmail.com>
2024-03-04 15:33:06 -0500

    Fixing the DataStorageIntegration test
    Got rid of the main method, edited the InputConfig to allow for different arguments as parameters.

--- test/DataStoreIntegration.java
+++ test/DataStoreIntegration.java
@@ -1,8 +1,8 @@
 class DataStoreIntegration {
-    public static void main(String args[]) throws Exception {
-        DataToFromComputeAPI api;
+    public void dataStoreIntegrationTest() throws Exception {
+        DataToFromComputeAPI api = new DataToFromComputeAPI(); // Initialize the api variable
         OutputConfig out;
-        InputConfig in;
+        InputConfig in = new InputConfig(argument1, argument2); // Initialize the in variable with the required arguments
 
         api.readData(null);
         api.writeData(in, null);
