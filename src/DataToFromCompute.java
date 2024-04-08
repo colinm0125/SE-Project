8facacb026bea800b1b5d279c0caa9574e3278f1
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-08 17:46:00 -0400

    typo

--- src/DataToFromCompute.java
+++ src/DataToFromCompute.java
@@ -1,6 +1,6 @@
 public interface DataToFromCompute {
     //A method to read the data from the source
-    List<Integer> readData(int);
+    List<Integer> readData(int key);
     //A method to write to the output source
     int writeData(List<Integer> data);
     //Ask Prof Abt source type as a parameter versus a return type.
