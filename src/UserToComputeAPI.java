0a715cb7e7cf29d5e8c12387cd6b7f8fec662af7
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-09 16:28:41 -0400

    Update UserToComputeAPI.java

--- src/UserToComputeAPI.java
+++ src/UserToComputeAPI.java
@@ -8,7 +8,7 @@ public class UserToComputeAPI implements UserToCompute{
     RawData input;
 
     //Constructors
-    public UserToComputeAPI(SourceType source, OutputDest dest, UserDelimeter inputDelim, UserDelimeter outputDelim, RawData input) {
+    public UserToComputeAPI(SourceType source, int dest, UserDelimeter inputDelim, UserDelimeter outputDelim, RawData input) {
         this.source = source;
         this.dest = dest;
         this.inputDelim = inputDelim;
