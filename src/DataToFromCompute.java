bdc60ca63a693c7f6d1b93d945495ca24da3039f
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-09 16:24:54 -0400

    Update DataToFromCompute.java

--- src/DataToFromCompute.java
+++ src/DataToFromCompute.java
@@ -1,9 +1,10 @@
 import java.util.List;
+import java.io.IOException;
 
 public interface DataToFromCompute {
     //A method to read the data from the source
-    List<Integer> readData(int key);
+    List<Integer> readData(int key) throws IOException;
     //A method to write to the output source
-    int writeData(List<Integer> data);
+    int writeData(List<Integer> data) throws IOException;
     //Ask Prof Abt source type as a parameter versus a return type.
 }
