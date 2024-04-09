fb2da1f6a1c5d64a161d90f7818f6d5d90a9d268
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-09 19:27:41 -0400

    Update DataStoreIntegration.java

--- test/DataStoreIntegration.java
+++ test/DataStoreIntegration.java
@@ -1,10 +1,11 @@
 import java.util.List;
+import java.util.ArrayList;
 
 class DataStoreIntegration {
     public void dataStoreIntegrationTest() throws Exception {
         DataToFromComputeAPI api = new DataToFromComputeAPI(); // Initialize the api variable
         OutputConfig out;
-        List<Integer> l = new List<>();
+        List<Integer> l = new ArrayList<Integer>();;
         InputConfig in = new InputConfig(l); // Initialize the in variable with the required arguments
 
         api.readData(0);
