830f64940e61dde48351c3796f6ed79b9af03607
Colin <134108049+colinm0125@users.noreply.github.com>
2024-04-09 19:34:49 -0400

    Update TestDataToFromComputeAPI.java

--- test/TestDataToFromComputeAPI.java
+++ test/TestDataToFromComputeAPI.java
@@ -8,7 +8,7 @@ public class TestDataToFromComputeAPI {
 
     @Test
     public void testWrite() throws Exception {
-        SourceType mockSource = Mockito.mock(SourceType.class);
+        int mockSource = Mockito.mock(Integer.class);
         ComputeResults mockResult = Mockito.mock(ComputeResults.class);
 
         when(testAPI.writeData(any(SourceType.class), any(ComputeResults.clas))).thenReturn("Key made");
@@ -18,9 +18,9 @@ public class TestDataToFromComputeAPI {
 
     @Test
     public void testRead() throws Exception {
-        SourceType mockSource = Mocktio.mock(SourceType.class);
+        int mockSource = Mocktio.mock(Integer.class);
 
-        when(testAPI.readData(any(SourceType.class))).thenReturn("Data read");
+        when(testAPI.readData(any(Integer.class))).thenReturn("Data read");
 
         testAPI.readData(mockSource);
     }
