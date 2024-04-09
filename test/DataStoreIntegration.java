f62367b9096c1995506e6ad4975ac49b4c460f42
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-09 19:13:38 -0400

    Update DataStoreIntegration.java

--- test/DataStoreIntegration.java
+++ test/DataStoreIntegration.java
@@ -5,7 +5,7 @@ class DataStoreIntegration {
         DataToFromComputeAPI api = new DataToFromComputeAPI(); // Initialize the api variable
         OutputConfig out;
         List<Integer> l = new List<>();
-        InputConfig in = new InputConfig(); // Initialize the in variable with the required arguments
+        InputConfig in = new InputConfig(l); // Initialize the in variable with the required arguments
 
         api.readData(0);
         api.writeData(in);
