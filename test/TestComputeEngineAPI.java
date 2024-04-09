237d55c0e1164cb966ab683de06e8b681ea470cb
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-09 18:51:45 -0400

    Update TestComputeEngineAPI.java

--- test/TestComputeEngineAPI.java
+++ test/TestComputeEngineAPI.java
@@ -14,7 +14,7 @@ public class TestComputeEngineAPI {
         List<Integer> data = Mockito.mock(List.class);
         ComputeEngineAPI testAPI = new ComputeEngineAPI();
 
-        when(testAPI.compute(any(List<Integer>))).thenReturn("Data computed");
+        when(testAPI.compute(any(List.class))).thenReturn("Data computed");
  
         testAPI.compute(data);
     }
