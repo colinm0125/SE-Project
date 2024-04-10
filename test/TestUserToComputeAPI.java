6ed94a810ceeafeb9308f13649277e42bef522d6
Colin <134108049+colinm0125@users.noreply.github.com>
2024-04-09 20:06:07 -0400

    Update TestUserToComputeAPI.java (#84)

--- test/TestUserToComputeAPI.java
+++ test/TestUserToComputeAPI.java
@@ -1,19 +1,24 @@
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
+import java.util.List;
 
 public class TestUserToComputeAPI {
      private UserToComputeAPI testAPI;
 
      public void testSetInputSource() throws Exception {
-        int sourceMock=Mockito.mock(Integer.class);
-        when(testAPI.setInputSource(any(Integer.class))).thenReturn("Setting Input Success");
+
+        List<Integer> sourceMock = Mockito.mock(Integer.class);
+        when(testAPI.setInputSource(any(SourceType.class))).thenReturn(sourceMock);
+
         testAPI.setInputSource(sourceMock);
     }
 
     public void testSetOutputDest() throws Exception {
-        int outputMock=Mockito.mock(Integer.class);
-        when(testAPI.setOutputDest(any(Integer.class))).thenReturn("Setting Output Dest Success");
+
+        int outputMock = Mockito.mock(Integer.class);
+        when(testAPI.setOutputDest(any(Integer.class))).thenReturn(outputMock);
+
         testAPI.setOutputDest(outputMock);
 
     }
@@ -21,17 +26,19 @@ public class TestUserToComputeAPI {
     public void testSetDelimeter() throws Exception {
         UserDelimeter inputDelimMock=Mockito.mock(UserDelimeter.class); 
         UserDelimeter outputDelimMock=Mockito.mock(UserDelimeter.class); 
-        when(testAPI.setDelimeter(any(UserDelimeter.class))).thenReturn("Setting delimeter success");
+        when(testAPI.setDelimeter(any(UserDelimeter.class))).thenReturn(inputDelimMock);
         testAPI.setDelimeter(inputDelimMock,outputDelimMock);
     }
 
     public void testGetOutput() throws Exception {
-        when(testAPI.getOutput()).thenReturn("Output :)");
+        List<Integer> outMock = Mockito.mock(List.class); 
+        when(testAPI.getOutput()).thenReturn(outMock);
         testAPI.getOutput();
     }
 
     public void testComputeRequest() throws Exception {
-        when(testAPI.computeRequest(any(RawData.class))).thenReturn("Compute Request success");
+        List<Integer> mockCompRequest 
+        when(testAPI.computeRequest(any(List.class))).thenReturn(mockCompRequest);
         testAPI.computeRequest();
     }
 }
