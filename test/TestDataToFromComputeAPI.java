7f14ccf996638acb5cbac42a7c0c0400c16c308f
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-09 20:09:02 -0400

    Update TestDataToFromComputeAPI.java

--- test/TestDataToFromComputeAPI.java
+++ test/TestDataToFromComputeAPI.java
@@ -2,6 +2,8 @@ import org.mockito.Mockito;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
+import java.util.List;
+import org.mockito.Mocktio.mock;
 
 public class TestDataToFromComputeAPI {
     private DataToFromComputeAPI testAPI;
@@ -10,18 +12,20 @@ public class TestDataToFromComputeAPI {
     public void testWrite() throws Exception {
         int mockSource = Mockito.mock(Integer.class);
         ComputeResults mockResult = Mockito.mock(ComputeResults.class);
+        List<Integer> fakeList = Mockito.mock(List.class);
 
-        when(testAPI.writeData(any(Integer.class), any(ComputeResults.class))).thenReturn("Key made");
+        when(testAPI.writeData(any(Integer.class)))).thenReturn(fakeList);
 
-        testAPI.writeData(mockSource, mockResult);
+        testAPI.writeData(0);
     }
 
     @Test
     public void testRead() throws Exception {
         int mockSource = Mocktio.mock(Integer.class);
+        List<Integer> fakeList = Mockito.mcok(List.class);
 
 
-        when(testAPI.readData(any(Integer.class))).thenReturn("Data read");
+        when(testAPI.readData(any(Integer.class))).thenReturn(fakeList);
 
         testAPI.readData(mockSource);
     }
