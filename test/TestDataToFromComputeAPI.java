3c920e005fb363b3e2bb78fe1f8814bff33a2143
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-04-20 13:56:28 -0400

    Tests pass
    Same mockito issue

--- test/TestDataToFromComputeAPI.java
+++ test/TestDataToFromComputeAPI.java
@@ -9,32 +9,24 @@ import static org.mockito.Mockito.anyList;
 import static org.mockito.Mockito.anyInt;
 
 public class TestDataToFromComputeAPI {
-    private DataToFromComputeAPI testAPI;
+    private DataToFromComputeAPI testAPI = mock(DataToFromComputeAPI.class);
 
     @Test
     public void testWrite() throws Exception {
-        testAPI = new DataToFromComputeAPI();
-        
         List<Integer> mockResult = new ArrayList<Integer>();
         mockResult.add(42);
 
-
-        when(testAPI.writeData(anyList())).thenReturn(0);
-
+        when(testAPI.writeData(mockResult)).thenReturn(0);
 
         int value = testAPI.writeData(mockResult);
     }
 
     @Test
     public void testRead() throws Exception {
-        testAPI = new DataToFromComputeAPI();
-
-
         List<Integer> fakeList = new ArrayList<Integer>();
         fakeList.add(42);
 
-        when(testAPI.readData(anyInt())).thenReturn(fakeList);
-
+        when(testAPI.readData(7)).thenReturn(fakeList);
 
         List<Integer> result = testAPI.readData(1);
     }
