2aec1c37c839ac59dfbcd0ec38d4dfa7bad69565
Colin <134108049+colinm0125@users.noreply.github.com>
2024-04-27 16:53:27 -0400

    Update FrontEnd.java

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FrontEnd {
-    public static void main(String[] args) {
+    public static void main(String[] args) throw IOException {
         List<Integer> dummyList = new ArrayList<Integer>();
         char delim = ' ';
         Coordination coordinator = new Coordination();
