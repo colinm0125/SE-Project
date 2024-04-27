858a5ca5a093f170f88fc883c4a5ffe8a6ca7ded
Colin <134108049+colinm0125@users.noreply.github.com>
2024-04-27 16:47:04 -0400

    Update FrontEnd.java

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class FrontEnd {
     public static void main(String[] args) {
-        List<Integer> dummyList = new List<Integer>();
+        ArrayList<Integer> dummyList = new ArrayList<Integer>();
         char delim = ' ';
         Coordination coordinator = new Coordination();
         Scanner in = new Scanner(System.in);
