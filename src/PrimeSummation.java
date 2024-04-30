75704f0c638ea1eaaab54334d1e27a7c358b0acd
Colin <134108049+colinm0125@users.noreply.github.com>
2024-04-30 16:23:57 -0400

    Update PrimeSummation.java

--- src/PrimeSummation.java
+++ src/PrimeSummation.java
@@ -1,49 +1,38 @@
+import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrimeSummation {
+public class PerformanceTest {
 
-    public PrimeSummation() {
-
-    }
-
-    public int master(int n) {
-        List<List<Integer>> primeSums = computePrimeSums(n);
-        return primeSums.size();
-    }
-
-    public static List<List<Integer>> computePrimeSums(int n) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> currentSum = new ArrayList<>();
-        generatePrimeSums(result, currentSum, n, 2);
-        return result;
-    }
-
-    public static void generatePrimeSums(List<List<Integer>> result, List<Integer> currentSum, int remaining, int start) {
-        if (remaining == 0) {
-            result.add(new ArrayList<>(currentSum));
-            return;
+    @Test
+    public void speedTest() {
+        Coordination cord = new Coordination();
+        List<Integer> in = new ArrayList<Integer>();
+        List<Integer> out = new ArrayList<Integer>();
+        in.add(1); in.add(4); in.add(10); in.add(84); in.add(44); in.add(98); in.add(301);
+        long avg = 0;
+        for(int i=0; i<=100; i++) {
+            avg+=cordSpeed(cord, in);
         }
-        for (int i = start; i <= remaining; i++) {
-            if (isPrime(i)) {
-                currentSum.add(i);
-                generatePrimeSums(result, currentSum, remaining - i, i);
-                currentSum.remove(currentSum.size() - 1);
-            }
+        System.out.println("Coordination speed "+avg/100);
+
+        ComputeEngineAPI puter = new ComputeEngineAPI();
+        long comavg = 0;
+        for(int i=0; i<=100; i++) {
+            comavg+=computeSpeed(puter, in);
         }
+        System.out.println("Compute engine speed "+comavg/100);
     }
-
-
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+    public long cordSpeed(Coordination cord, List<Integer> in) {
+        long startTime = System.currentTimeMillis();
+        cord.computeRequest(in);
+        long endTime = System.currentTimeMillis();
+        return endTime-startTime;
+    }
+    public long computeSpeed(ComputeEngineAPI puter, List<Integer> in) {
+        long startTime=System.currentTimeMillis();
+        puter.compute(in);
+        long endTime=System.currentTimeMillis();
+        return endTime-startTime;
     }
 }
-
