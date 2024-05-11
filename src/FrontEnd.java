15638934346518fe23244fceeef99961209d65e5
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-05-10 21:48:57 -0400

    Updated FrontEnd.java to have gui (#132)
    * Updated FrontEnd.java to have gui
    
    Added a gui to the front end which includes JFileChooser, a cool java swing library that makes selecting files easier. https://docs.oracle.com/javase/8/docs/api/javax/swing/JFileChooser.html
    
    * Updated FrontEnd.java imports
    
    fixed imports since checkstyle doesn't like *

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,46 +1,118 @@
-import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JFileChooser;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 
 public class FrontEnd {
+
+    // classic java gui declarations
+    private static JFrame frame;
+    private static JPanel outputPanel;
+    private static JPanel panel;
+    private static JTextField selectedFileField;
+    private static JTextField outputFileField;
+    private static JTextField outputField;
+    private static JButton selectFileButton;
+    private static JButton computeButton;
+    private static JLabel outputLabel;
+
+    private static String selectedFilePath;
+    private static String outputFilePath;
+
     public static void main(String[] args) throws IOException {
         List<Integer> dummyList = new ArrayList<Integer>();
-        char delim = ' ';
         Coordination coordinator = new Coordination();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the source type. Enter 1 for file, Enter 2 for manual input: ");
-        int inputType=in.nextInt();
-        
-        //Receive input from file
-        if(inputType==1) {
-            System.out.println("Enter the file path: ");
-            String path = in.next();
-            DataToFromComputeAPI data = new DataToFromComputeAPI();
-            List<Integer> list = data.readData(path);
-            dummyList = coordinator.computeRequest(list);
-        } else if(inputType==2) {
-            //Receive input from user
-            System.out.println("Enter the delimeter: ");
-            if(in.nextLine()!=null) {
-                delim = in.next().charAt(0);
-            } else {
-                delim = ',';
+
+        frame = new JFrame("File Selector");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 1));
+
+        // Selected File Field
+        selectedFileField = new JTextField();
+        selectedFileField.setEditable(false);
+        panel.add(selectedFileField);
+
+        // Output File Field
+        outputFileField = new JTextField();
+        outputFileField.setEditable(false);
+        panel.add(outputFileField);
+
+        // Select File Button
+        selectFileButton = new JButton("Select File");
+        selectFileButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                selectedFilePath = selectFile();
+                selectedFileField.setText(selectedFilePath);
             }
-            System.out.println("Enter list of positive non zero integers, separated by chosen delimeter: ");
-            String input = in.next();
-            String[] numbers = input.split(String.valueOf(delim));
-            List<Integer> list = new ArrayList<>();
-            for (String number : numbers) {
-                int value = Integer.parseInt(number);
-                list.add(value);
+        });
+        panel.add(selectFileButton);
+
+        // Compute Button
+        computeButton = new JButton("Compute");
+        panel.add(computeButton);
+        frame.getContentPane().add(panel);
+        frame.setSize(400, 250);
+        frame.setVisible(true);
+        computeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)  {
+                frame.getContentPane().removeAll();
+                outputPanel = new JPanel();
+                outputPanel.setLayout(new GridLayout(4, 1));
+                String out = "";
+                List<Integer> list;
+                DataToFromComputeAPI data = new DataToFromComputeAPI();
+                try {
+                    list = data.readData(selectedFilePath);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                // dont worry about this...
+                list.add(0); list.add(0);
+
+                List<Integer> outputList = coordinator.computeRequest(list);
+                if(!outputList.isEmpty()) {
+                    for(int i=0; i<outputList.size()-1; i++){
+
+                        // prevents text from ending with extra ,
+                        if(i == outputList.size()-2) {
+                            out+=outputList.get(i)+" ";
+                        } else {
+                            out+=outputList.get(i)+", ";
+                        }
+                    }
+                    outputField = new JTextField(out);
+                } else {
+                    outputField = new JTextField("File appears to be empty");
+                }
+
+                outputPanel.add(outputField);
+                frame.add(outputPanel);
+                frame.repaint();
+                frame.revalidate();
             }
-            
-            dummyList = coordinator.computeRequest(list);
-        } else {
-            System.out.println("Invalid input. Please enter 1 or 2.");
+        });
+        frame.setVisible(true);
+    }
+
+    private static String selectFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            return selectedFile.getAbsolutePath();
         }
-        
-        in.close();
+        return "No File Chosen";
     }
 }
