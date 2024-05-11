658750646077e42f111910cc219f617723edeebe
Brian  Abbott <113200930+babbo65@users.noreply.github.com>
2024-05-10 21:31:36 -0400

    Updated FrontEnd.java imports
    fixed imports since checkstyle doesn't like *

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,6 +1,12 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JFileChooser;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
